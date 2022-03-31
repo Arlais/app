2f2357619b4f8b83a512a479bc40a29303636499
Jonathan Pulido <jonathan.pulido@mail.escuelaing.edu.co>
2022-03-31 14:02:17 -0500

    proyecto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/Landing";
 import { useReducer } from "react";
 import { initialState, themeReducer } from "./utils";
 import { ThemeContext } from "./ThemeContext";
+import Payment from "./components/Payment";
+import Settings from "./components/Settings";
 
 function App() {
   const [state, dispatch] = useReducer(themeReducer, initialState);
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="login" element={<Login />} />
           <Route path="landing" element={<Landing />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </ThemeContext.Provider>
